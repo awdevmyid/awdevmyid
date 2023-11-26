e325c87bef99207745d3e5f67c224fa2958f7959
awdevmyid <144774921+awdevmyid@users.noreply.github.com>
2023-11-26 07:02:50 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+const Mustache = require('mustache');
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+let DATA = {
+  refresh_date: new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'Europe/Istanbul',
+  }),
+};
+
+async function setWeatherInformation() {
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+  )
+    .then(r => r.json())
+    .then(r => {
+      DATA.city_temperature = Math.round(r.main.temp);
+      DATA.city_weather = r.weather[0].description;
+      DATA.city_weather_icon = r.weather[0].icon;
+      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Europe/Istanbul',
+      });
+      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Europe/Istanbul',
+      });
+    });
+}
+
+
+
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+
+async function action() {
+  /**
+   * Fetch Weather
+   */
+  await setWeatherInformation();
+
+  /**
+   * Generate README
+   */
+  await generateReadMe();
+
+}
+
+action();
